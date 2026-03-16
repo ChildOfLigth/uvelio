e860a411f60243d10952dae9300a53fed2819925
Kovtun Sofia <kovtuns783@gmail.com>
2026-03-16 17:58:30 +0200

    Completion of the Main page development, making changes to the ProductPage component

--- client/src/components/pages/Main.jsx
+++ client/src/components/pages/Main.jsx
@@ -1,35 +1,67 @@
-import decorateRing from "@imgs/decorateRingForPresentBlock.png";
-import weddingRing from "@imgs/weddingRing.png";
-import classicRing from "@imgs/classicRing.png";
-import ringWithInlay from "@imgs/ringWithInlay.png";
-import ringWithoutInlay from "@imgs/ringWithoutInlay.png";
-import laboratoryEcoDimond from "@imgs/laboratoryEcoDimond.png";
-import exclusiveJewelry from "@imgs/exclusiveJewelry.png";
 import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import decorateRing from "@imgs/decorateRingForPresentBlock.webp";
+import weddingRing from "@imgs/weddingRing.webp";
+import classicRing from "@imgs/classicRing.webp";
+import ringWithInlay from "@imgs/ringWithInlay.webp";
+import ringWithoutInlay from "@imgs/ringWithoutInlay.webp";
+import laboratoryEcoDimond from "@imgs/laboratoryEcoDimond.webp";
+import exclusiveJewelry from "@imgs/exclusiveJewelry.webp";
+import uvelioIconWhiteColor from "@imgs/icons/uvelioWhiteColor.svg";
+import arrowIcoWhiteColor from "@imgs/icons/arrowIcoWhiteColor.svg";
+import earrings from "@imgs/categoryEarrings.webp";
+import chains from "@imgs/categoryChains.webp";
+import bracelets from "@imgs/categoryBracelets.webp";
+import necklaces from "@imgs/categoryNecklaces.webp";
+import pendants from "@imgs/categoryPendants.webp";
+import crosses from "@imgs/categoryCrosses.webp";
+import brooches from "@imgs/categoryBrooches.webp";
+import NavToCatalog from "@ui/NavToCatalog";
+import SwiperCatalog from "../ui/Swiper/SwiperCatalog";
 
 export default function Main() {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const newArrivalsProduct = useSelector(
+    (state) => state.products.productsList,
+  ).slice(0, 8);
+
+  const cardsForFilterCategory = [
+    { imgsSrc: earrings, title: t("mainPage.category.filterLinks.earrings") },
+    { imgsSrc: chains, title: t("mainPage.category.filterLinks.chains") },
+    { imgsSrc: bracelets, title: t("mainPage.category.filterLinks.bracelets") },
+    { imgsSrc: necklaces, title: t("mainPage.category.filterLinks.necklaces") },
+    { imgsSrc: pendants, title: t("mainPage.category.filterLinks.pendants") },
+    { imgsSrc: crosses, title: t("mainPage.category.filterLinks.crosses") },
+    { imgsSrc: brooches, title: t("mainPage.category.filterLinks.brooches") },
+  ];
 
   return (
     <>
       <div
-        className="flex items-center justify-center w-full bg-(--color-primary) overflow-hidden h-[60vh] md:h-[65vh] lg:h-[75vh] 
-      max-h-190 min-h-175 relative"
+        className="flex items-center justify-center w-full bg-(--color-primary) overflow-hidden h-[60svh] md:h-[70svh] lg:h-[75svh] 
+      max-h-190 relative"
       >
         <div className="block md:hidden w-full h-full absolute bg-(--color-primary)/40 z-20"></div>
 
         <div className="flex items-center justify-center relative w-[90%] z-30 md:justify-start">
           <div className="flex flex-col items-start justify-center gap-6 relative text-white w-full md:w-2/3 lg:w-1/2">
-            <h1 className="inline text-white font-medium">
+            <h1
+              className={`inline text-white font-medium ${i18n.language == "ua" ? "h1FszLangUa" : ""}`}
+            >
               {t("mainPage.presentBlock.title")}
             </h1>
             <p className="inline-block w-full text-2xl sm:w-2/3">
               {t("mainPage.presentBlock.subtitle")}
             </p>
 
-            <button className="bg-(--second-primary-с) w-2/5 min-w-30.5 xl:w-1/4 h-12.5 text-(--color-primary) p-4.25 customButtons">
-              <p>{t("mainPage.presentBlock.btnNavToCatalog")}</p>
-            </button>
+            <Link
+              to="/uvelio/catalog"
+              className={`bg-(--second-primary-с) ${i18n.language == "ua" ? "xs:w-auto" : "w-2/5 min-w-30.5 xl:w-1/4"} 
+            h-12.5 text-(--color-primary) text-(length:--text-sm) p-4.25 customButton`}
+            >
+              {t("mainPage.presentBlock.btnNavToCatalog")}
+            </Link>
           </div>
         </div>
 
@@ -43,138 +75,168 @@ export default function Main() {
         />
       </div>
 
-      <div className="w-[90%] flex flex-col items-center justify-center mx-auto">
-        <div className="w-full flex items-center justify-center flex-col gap-11 my-[clamp(50px,10vw,150px)] max-h-173.75">
-          <nav className="w-full grid grid-cols-2 gap-3 md:grid-cols-4 xl:h-116">
-            <div
-              className="bg-white rounded-(--border-radius) pt-3 flex flex-col items-start justify-between overflow-hidden
-          col-span-1 md:h-75 md:grid-cols-1"
+      <div className="w-[91.7%] max-w-420 flex flex-col items-center justify-center mx-auto">
+        <section className="w-full flex items-center justify-center flex-col gap-[clamp(1.25rem,calc(0.714rem+2.679vw),3.125rem)] my-[clamp(3.125rem,calc(1.696rem+7.143vw),8.125rem)]">
+          <nav className="w-full grid grid-cols-2 gap-3 xl:flex xl:justify-center x:gap-5 xl:h-116">
+            <NavToCatalog
+              filter="rings"
+              styles="bg-white rounded-(--border-radius) pt-3 flex flex-col items-start justify-between gap-5 overflow-hidden
+          col-span-1 xl:h-full xl:flex-[38%]"
             >
-              <h2 className="text-(--color-primary) mx-auto">
+              <h3 className="text-(--color-primary) mx-auto">
                 {t("mainPage.navToCatalogWithFilter.rings")}
-              </h2>
-              <img
-                src={classicRing}
-                alt="Ring"
-                className="w-auto h-[60%] sm:h-[75%]"
-              />
-            </div>
+              </h3>
 
-            <div
-              className="bg-white rounded-(--border-radius) pt-3 flex flex-col items-start justify-between overflow-hidden
-          col-span-1 md:h-75 md:grid-cols-1"
+              <img src={classicRing} alt="Ring" className="w-[80%] xl:w-full" />
+            </NavToCatalog>
+
+            <NavToCatalog
+              filter="rings?type=wedding"
+              styles="bg-white rounded-(--border-radius) pt-3 flex flex-col items-start justify-between gap-5 overflow-hidden
+          col-span-1 xl:h-full xl:flex-[33%]"
             >
-              <h2 className="text-(--color-primary) text-center mx-auto">
+              <h3 className="text-(--color-primary) text-center mx-auto">
                 {t("mainPage.navToCatalogWithFilter.wedRings")}
-              </h2>
+              </h3>
+
               <img
                 src={weddingRing}
                 alt="Wedding rings"
                 className="w-auto h-[70%] sm:h-[75%]"
               />
-            </div>
+            </NavToCatalog>
 
-            <div className="flex flex-col items-center gap-3 col-span-2 md:flex-col">
-              <div
-                className="bg-white w-full h-[48%] rounded-(--border-radius) flex items-center 
-            justify-center pl-5 relative min-h-25 min-[500px]:min-h-35 lg:pr-5 lg:pl-0 lg:justify-end"
+            <div className="flex flex-col items-center gap-3 xl:gap-5 col-span-2 xl:flex-[39%] md:flex-row xl:flex-col">
+              <NavToCatalog
+                filter="rings?insert=true"
+                styles="bg-white w-full h-[48%] rounded-(--border-radius) flex items-center 
+            justify-center pl-5 relative min-h-25 overflow-hidden min-[500px]:min-h-35 xl:pr-5 xl:pl-0 xl:justify-end"
               >
                 <img
                   src={ringWithInlay}
                   alt="Ring with insert"
                   className="aspect-squere h-full absolute left-0 z-10"
                 />
-                <h2 className="text-(--color-primary) z-30">
-                  {t("mainPage.navToCatalogWithFilter.withIsert")}
-                </h2>
-              </div>
 
-              <div
-                className="bg-white w-full h-[48%] rounded-(--border-radius) flex items-center 
-            justify-center pl-5 relative min-h-25 min-[500px]:min-h-35 lg:pr-5 lg:pl-0 lg:justify-end"
+                <h3 className="text-(--color-primary) z-30">
+                  {t("mainPage.navToCatalogWithFilter.withIsert")}
+                </h3>
+              </NavToCatalog>
+
+              <NavToCatalog
+                to="rings?insert=false"
+                styles="bg-white w-full h-[48%] rounded-(--border-radius) flex items-center 
+            justify-center pl-5 relative min-h-25 overflow-hidden min-[500px]:min-h-35 xl:pr-5 xl:pl-0 xl:justify-end"
               >
                 <img
                   src={ringWithoutInlay}
                   alt="Ring without insert"
                   className="w-45% h-full absolute left-0 z-10"
                 />
-                <h2 className="text-(--color-primary) z-30">
+
+                <h3 className="text-(--color-primary) z-30">
                   {t("mainPage.navToCatalogWithFilter.withoutIsert")}
-                </h2>
-              </div>
+                </h3>
+              </NavToCatalog>
             </div>
           </nav>
 
           <div className="flex items-center justify-between w-[16%] min-w-52.75">
-            <button className="bg-(--color-primary) flex items-center justify-center gap-2.5 w-[45%] p-4 rounded-(--border-radius)">
-              <p className="text-white flex items-center leading-normal">
-                Gold
-              </p>
-              <svg
-                width="7"
-                height="13"
-                viewBox="0 0 7 13"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-[80%]"
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M6.83814 5.89967C7.05396 6.11934 7.05396 6.47544 6.83814 6.69512L1.2039 12.43C0.988082 12.6496 0.638231 12.6496 0.422411 12.43L0.161865 12.1648C-0.053955 11.9451 -0.053955 11.589 0.161865 11.3693L5.14481 6.29739L0.161865 1.22544C-0.053955 1.00577 -0.053955 0.649666 0.161865 0.429991L0.422411 0.164791C0.638231 -0.054884 0.988082 -0.054884 1.2039 0.164791L6.83814 5.89967Z"
-                  fill="#ffffff"
-                />
-              </svg>
-            </button>
+            <NavToCatalog
+              to="?material=gold"
+              styles={`bg-(--color-primary) flex items-center justify-center gap-2.5 ${i18n.language == "ua" ? "w-auto" : "w-[45%]"} p-4 
+              rounded-(--border-radius) text-white`}
+            >
+              {t("mainPage.filterCatalogByMaterial.gold")}
+              <img src={arrowIcoWhiteColor} />
+            </NavToCatalog>
 
-            <button className="bg-(--color-primary) flex items-center justify-center gap-2.5 w-[45%] p-4 rounded-(--border-radius)">
-              <p className="text-white flex items-center leading-normal">
-                Silver
-              </p>
-              <svg
-                width="7"
-                height="13"
-                viewBox="0 0 7 13"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-[80%]"
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M6.83814 5.89967C7.05396 6.11934 7.05396 6.47544 6.83814 6.69512L1.2039 12.43C0.988082 12.6496 0.638231 12.6496 0.422411 12.43L0.161865 12.1648C-0.053955 11.9451 -0.053955 11.589 0.161865 11.3693L5.14481 6.29739L0.161865 1.22544C-0.053955 1.00577 -0.053955 0.649666 0.161865 0.429991L0.422411 0.164791C0.638231 -0.054884 0.988082 -0.054884 1.2039 0.164791L6.83814 5.89967Z"
-                  fill="#ffffff"
-                />
-              </svg>
-            </button>
+            <NavToCatalog
+              to="?material=silver"
+              styles={`bg-(--color-primary) flex items-center justify-center gap-2.5 ${i18n.language == "ua" ? "w-auto" : "w-[45%]"} p-4 
+              rounded-(--border-radius) text-white`}
+            >
+              {t("mainPage.filterCatalogByMaterial.silver")}
+              <img src={arrowIcoWhiteColor} />
+            </NavToCatalog>
           </div>
-        </div>
+        </section>
 
-        <div className="w-full h-auto flex-row items-center justify-center text-white">
-          <section className="bg-[url(@imgs/exclusiveJewelry.png)] w-[48%] flex justify-start items-center py-[clamp(30px,7vw,100px)] pl-6 rounded-(--border-radius)">
-            <div className="w-2/3">
-              <h2>{t("mainPage.advertisement.ecoDiamonds.title")}</h2>
-              <p className="py-9">
+        <section className="w-full max-w-420 flex flex-col items-start justify-center m-[clamp(30px,6vw,70px)]">
+          <SwiperCatalog
+            catalog={newArrivalsProduct}
+            titleSwiper={t("mainPage.newArrivalsTitle")}
+          />
+        </section>
+
+        <section className="w-full flex flex-col md:flex-row gap-[clamp(5px,3vw,27px)] items-stretch justify-between text-white">
+          <div className="w-full md:w-[48.65%] flex justify-start items-center relative py-[clamp(30px,7vw,93.5px)] rounded-(--border-radius) overflow-hidden">
+            <img
+              src={laboratoryEcoDimond}
+              className="w-full h-full object-cover absolute"
+              alt="Jewelry with eco diamonds"
+            />
+
+            <div className="w-5/6 pl-5 relative flex flex-col items-start justify-center">
+              <h3>{t("mainPage.advertisement.ecoDiamonds.title")}</h3>
+              <p className="py-9 text-(length:--base-p-size)">
                 {t("mainPage.advertisement.ecoDiamonds.subtitle")}
               </p>
               <button className="bg-(--second-primary-с) rounded-(--border-radius) p-4.25">
                 {t("mainPage.advertisement.ecoDiamonds.textBtn")}
               </button>
             </div>
-          </section>
+          </div>
 
-          <section className="bg-[url(@imgs/laboratoryEcoDimond.png)] w-[48%] flex justify-start items-center pl-6 rounded-(--border-radius)">
-            <div className="w-2/3">
-              <h2>{t("mainPage.advertisement.exlusive.title")}</h2>
-              <p className="py-9">
+          <div className="w-full md:w-[48.65%] flex justify-start items-center relative py-[clamp(30px,7vw,93.5px)] rounded-(--border-radius) overflow-hidden">
+            <img
+              src={exclusiveJewelry}
+              className="w-full h-full object-cover absolute"
+              alt="Exclusive jewerly"
+            />
+
+            <div className="w-5/6 pl-5 relative flex flex-col items-start justify-center">
+              <div className="w-1/2">
+                <img src={uvelioIconWhiteColor} className="w-full min-w-38" />
+                <h3>{t("mainPage.advertisement.exlusive.title")}</h3>
+              </div>
+
+              <p className="py-9 text-(length:--base-p-size)">
                 {t("mainPage.advertisement.exlusive.subtitle")}
               </p>
               <button className="bg-(--second-primary-с) rounded-(--border-radius) p-4.25">
                 {t("mainPage.advertisement.exlusive.textBtn")}
               </button>
             </div>
-          </section>
-        </div>
+          </div>
+        </section>
+
+        <section className="w-full my-[clamp(1.875rem,calc(-0.089rem+9.821vw),8.75rem)] flex flex-col items-start justify-center gap-6">
+          <h2>{t("mainPage.category.title")}</h2>
+
+          <ul className="w-full flex justify-start mx-2 md:grid md:grid-cols-4 2xl:grid-cols-5 items-stretch gap-3 md:gap-4 overflow-x-scroll disableScrollbar">
+            {cardsForFilterCategory.map((item) => (
+              <li key={item.title}>
+                <NavToCatalog
+                  filter={item.title}
+                  styles="w-50 md:w-full max-w-[320px] md:col-span-1 h-auto max-h-100.25 rounded-(--border-radius) bg-white flex flex-col items-center justify-center overflow-hidden"
+                >
+                  <img
+                    src={item.imgsSrc}
+                    alt={item.title}
+                    className="w-full h-[60%]"
+                  />
+
+                  <div className="w-full h-[40%] flex items-center justify-center bg-(--color-primary) py-[clamp(1.5625rem,calc(1.294rem+1.339vw),2.5rem)]">
+                    <p className="text-white text-base font-normal lg:text-xl md:font-bold uppercase text-center">
+                      {item.title}
+                    </p>
+                  </div>
+                </NavToCatalog>
+              </li>
+            ))}
+          </ul>
+        </section>
       </div>
     </>
   );
